1036dc412e8e1284047ece462036762d54ca2299
Alan Herculino <alan.siqueira.25782@aluno.facape.br>
2026-03-09 11:23:08 -0300

    feat: initial page structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import './Style.css'
+import Header from './Components/Header';
+import Sidenav from './Components/Sidenav';
+import Resumo from './Pages/Resumo';
+import './Style.css';
 
 function App() {
-  
-
   return (
     <div>
+      <div>
+        <Sidenav />
+      </div>
+      <main>
+        <Header />
+        <Resumo />
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
