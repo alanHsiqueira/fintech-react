4adfa85fc48647b3ccc4c35d78c5e1daa25506e4
Alan Herculino <alan.siqueira.25782@aluno.facape.br>
2026-03-10 11:04:21 -0300

    feat: add reactive date

--- src/Components/DateRange.tsx
+++ src/Components/DateRange.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import DateInput from './DateInput';
+import { useData } from '../Context/DataContext';
 
 const DateRange = () => {
-
+  const {inicio, setInicio, final, setFinal} = useData()
   return (
     <form className='box flex' onSubmit={(e) => e.preventDefault}>
       <DateInput
@@ -10,14 +11,14 @@ const DateRange = () => {
         value={inicio}
         onChange={({ target }) => setInicio(target.value)}
       />
-      {inicio}
+
 
       <DateInput
         label="Final"
         value={final}
         onChange={({ target }) => setFinal(target.value)}
       />
-      {final}
+     
     </form>
   );
 };
