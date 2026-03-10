096d0780945e023f9fdcf2fecab4c7cfc046d4b7
Alan Herculino <alan.siqueira.25782@aluno.facape.br>
2026-03-10 15:39:39 -0300

    feat: add month button

--- src/Components/DateRange.tsx
+++ src/Components/DateRange.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
 import DateInput from './DateInput';
 import { useData } from '../Context/DataContext';
 
 const DateRange = () => {
-  const {inicio, setInicio, final, setFinal} = useData()
+  const { inicio, setInicio, final, setFinal } = useData();
   return (
-    <form className='box flex' onSubmit={(e) => e.preventDefault}>
+    <form className="box flex" onSubmit={(e) => e.preventDefault}>
       <DateInput
         label="Inicio"
         value={inicio}
         onChange={({ target }) => setInicio(target.value)}
       />
 
-
       <DateInput
         label="Final"
         value={final}
         onChange={({ target }) => setFinal(target.value)}
       />
-     
     </form>
   );
 };
